8302b41bc05a4bf36bb18f71fee5ffef521ebaf1
jacobmoos <jacob.didoff@gmail.com>
2015-04-01 13:30:55 +0200

    afd
    weifn

--- WakeUp/app/src/main/java/thorleifz/wakeup/MainActivity.java
+++ WakeUp/app/src/main/java/thorleifz/wakeup/MainActivity.java
@@ -19,8 +19,8 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        return true; // commmenetnt
+    } // iwenivweiniwenv
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
