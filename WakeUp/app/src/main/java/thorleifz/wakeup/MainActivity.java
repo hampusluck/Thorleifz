dbe9b12eaac579676ecfc1a3eb2edc6686020332
adammyren <adammyren@hotmail.com>
2015-04-22 13:45:33 +0200

    added change activity intent

--- WakeUp/app/src/main/java/thorleifz/wakeup/MainActivity.java
+++ WakeUp/app/src/main/java/thorleifz/wakeup/MainActivity.java
@@ -1,9 +1,12 @@
 package thorleifz.wakeup;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +38,11 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void ButtonPressed(View v){
+
+        Intent theIntent = new Intent(this, LogIn.class);
+        startActivity(theIntent);
     }
 }
