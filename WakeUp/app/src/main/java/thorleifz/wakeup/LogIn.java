13bcca19ba339f7deb9621c5da0da265d6af4561
jacobmoos <jacob.didoff@gmail.com>
2015-04-22 11:33:08 +0200

    New login screen
    Some buttons for trial

---
+++ WakeUp/app/src/main/java/thorleifz/wakeup/LogIn.java
@@ -0,0 +1,17 @@
+package thorleifz.wakeup;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Jacob on 2015-04-22.
+ */
+public class LogIn extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+
+    }
+}
