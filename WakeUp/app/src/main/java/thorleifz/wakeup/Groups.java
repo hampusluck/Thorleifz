58844597f6dc624f24c8a7bcf1d70d487ff2ba09
adammyren <adammyren@hotmail.com>
2015-05-06 14:05:38 +0200

    Buggfix

--- WakeUp/app/src/main/java/thorleifz/wakeup/Groups.java
+++ WakeUp/app/src/main/java/thorleifz/wakeup/Groups.java
@@ -29,7 +29,7 @@ public class Groups extends ActionBarActivity {
    // private ArrayAdapter arrayAdapter;
     private String groupString; // a single string that contains all group names
     private String[] groupArray; // a single array that contains all group names
-
+    ArrayList<GroupClass> theList;
 
     private GroupListItemAdapter theAdapter;
     //private ArrayAdapter adapter;
@@ -38,18 +38,19 @@ public class Groups extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.group_screen);
-        groupArray = new String[]{"Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"};
+/*        groupArray = new String[]{"Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"};*/
         groupString = getIntent().getStringExtra("groups");
+        theList = new ArrayList<GroupClass>();
         if(groupString!=null) {
             fillArrayFromString(groupString);
         }
         groupList = (ListView) findViewById(R.id.groupList);
 
         //Skapar en lista med GroupClass objekt, varje GroupClass objekt innehåller information om en rad
-        ArrayList<GroupClass> theList = new ArrayList<GroupClass>();
-        theList.add(new GroupClass(R.drawable.imup,"TestGroup1","21:00"));
+
+/*        theList.add(new GroupClass(R.drawable.imup,"TestGroup1","21:00"));
         theList.add(new GroupClass(R.drawable.lozin,"TestGroup2","20:00"));
-        theList.add(new GroupClass(R.drawable.imup,"TestGroup3","19:00"));
+        theList.add(new GroupClass(R.drawable.imup,"TestGroup3","19:00"));*/
 
         //Initierar adaptern och skickar med listan så att den vet vad den ska fylla raderna med
         theAdapter = new GroupListItemAdapter(getApplicationContext(), R.layout.list_element, theList);
@@ -70,16 +71,13 @@ public class Groups extends ActionBarActivity {
         groupList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-<<<<<<< HEAD
-                String groupName = (String) groupList.getItemAtPosition(position);
+                GroupClass groupClass = (GroupClass) groupList.getItemAtPosition(position);
+                String groupName = groupClass.getGroup_name();
                 if(groupName!=null){
                 Intent intent = new Intent(Groups.this, InsideGroup.class);
                 intent.putExtra("groupName", groupName);
                 startActivity(intent);}
-=======
-                GroupClass s = (GroupClass) groupList.getItemAtPosition(position);
-                Toast.makeText(Groups.this,   " selected", Toast.LENGTH_LONG).show();
->>>>>>> featureGroupAdapter
+
             }
         });
 
@@ -109,9 +107,8 @@ public class Groups extends ActionBarActivity {
     //Takes a string of several group names and stores them in the groupArray
     private void fillArrayFromString(String s){
         Scanner sc = new Scanner(s);
-        int i = 0;
         while(sc.hasNext()){
-            groupArray[i++] = sc.next();
+            theList.add(new GroupClass(R.drawable.lozin, sc.next(), "11:00"));
         }
 
     }
